f27b6a026986b8bff3da8eeafafe5c8e9923ebdf
Michelle Liao <michelle.liao.93@gmail.com>
2016-03-02 23:30:13 -0500

    static url change for heroku

--- task/urls.py
+++ task/urls.py
@@ -3,6 +3,7 @@ from django.conf.urls import patterns, url
 from . import views
 
 
+
 urlpatterns = patterns(
     'task.views',
     url(r'^$', 'home'),
--- taskproject/urls.py
+++ taskproject/urls.py
@@ -15,8 +15,13 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
+from . import settings
+
 
 urlpatterns = [
 	url(r'^', include('task.urls')), #include task urls.py
     url(r'^admin/', admin.site.urls),
+    url(r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_URL}),
 ]
+
+
